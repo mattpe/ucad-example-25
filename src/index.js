568ea8fdd529841e6f983bdec5d6a83a495dd69f
mattpe <mattpe@metropolia.fi>
2025-10-21 15:12:51 +0300

    implement item retrieval and addition in API; add test requests; wip: get item by id

--- src/index.js
+++ src/index.js
@@ -2,15 +2,47 @@ import http from 'http';
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const items = [
+  {id: 10, name: 'Item1'},
+  {id: 27, name: 'Item2'},
+];
+
 const server = http.createServer((req, res) => {
   console.log(`HTTP request: ${req.method} ${req.url}`);
 
-  if (req.method === 'GET' && req.url === '/kukkuu') {
+  // GET all items
+  if (req.method === 'GET' && req.url === '/items') {
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('{"message": "no kukkuu!"}');
+    res.end(JSON.stringify(items));
+  // GET item by id
+  } else if (req.method === 'GET' && req.url.split('/')[1] === 'items') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const requestedID = req.url.split('/')[2]
+    // TODO: use request id to get the correct item object and add the object
+    // to the response
+    // TODO2: response 404 if object with the id does not exist
+    res.end('{"requestedID": ' + requestedID + '}');
+  // POST new item
+  } else if (req.method === 'POST' && req.url === '/items') {
+    let body = [];
+    req
+      .on('data', (chunk) => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+        console.log('req body', body);
+        const newItem = JSON.parse(body);
+        // check latest id and add 1
+        newItem.id = items[items.length-1].id + 1;
+        items.push(newItem);
+        res.statusCode = 201;
+        res.end();
+      });
   } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('Not found!!!');
+    res.statusCode = 404;
+    res.end();
   }
 });
 
