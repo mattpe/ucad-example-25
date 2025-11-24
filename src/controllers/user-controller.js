51345a3399dba676373c1d4b044fd44244ca7849
mattpe <mattpe@metropolia.fi>
2025-11-24 14:56:17 +0200

    add bcrypt password hashing

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import {addUser, selectUserByUsername} from '../models/user-model.js';
 import 'dotenv/config';
 
 const postLogin = async (req, res) => {
   console.log('postLogin', req.body);
   const user = await selectUserByUsername(req.body.username);
-  const passwordMatch = user && user.password === req.body.password;
+  const passwordMatch = user && await bcrypt.compare(req.body.password, user.password);
   if (passwordMatch) {
     // DO NOT send password to client
     delete user.password;
@@ -22,6 +23,8 @@ const postUser = async (req, res, next) => {
   const newUser = req.body;
   // regular user level by default
   newUser.user_level_id = 1;
+  // replace plain text pw with hashed one
+  newUser.password = await bcrypt.hash(newUser.password, 10);
   const result = await addUser(newUser);
   if (result.user_id) {
     return res.json({message: 'User created.', user_id: result.user_id});
